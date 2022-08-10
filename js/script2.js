53f1505f9dd956d21bec7260e59c110eb516da6b
RAFAEL BARTH OLIVEIRA <rafaboliveira1234525@gmail.com>
2022-08-10 16:50:21 -0300

    pagina7 importante

---
+++ js/script2.js
@@ -0,0 +1,4 @@
+function peganome() {
+    var recnome = document.formdados.camponome.value
+    alert("bem vindo " + recnome)
+}
---
+++ js/script3.js
@@ -0,0 +1,8 @@
+function acesso() {
+    var recidade = document.formidade.campoidade.value
+    if (recidade >= 18) {
+        window.open("https://youtube.com")
+    } else{
+        alert("voce não é digno\n sai fora...")
+    }
+}
