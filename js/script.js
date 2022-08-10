22c5aaeb35e2c3f8e91319efcac04da183f59fbf
RAFAEL BARTH OLIVEIRA <rafael.4981@aluno.pr.senac.br>
2022-08-10 14:29:38 -0300

    Exercicios JS

---
+++ js/script.js
@@ -0,0 +1,14 @@
+function correta(x) {
+    var teste=x
+    alert("Parabéns, a resposta correta é a letra" +teste)
+}
+function incorreta() {
+    alert("que pena, a resposta errada")
+}
+function correta(x) {
+    var teste=x
+    alert("Parabéns, a resposta correta é a letra" +teste)
+}
+function incorreta() {
+    alert("que pena, a resposta errada")
+}
