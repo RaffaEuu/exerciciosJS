f06e4bca902a50e1150b8bb04ceea7c0d62c762e
RAFAEL BARTH OLIVEIRA <rafaboliveira1234525@gmail.com>
2022-08-15 16:46:54 -0300

    olhar

---
+++ js/mediaNota.js
@@ -0,0 +1,15 @@
+function media() {
+    var nota1 = Number(document.peganotas.camponota1.value)
+    var nota2 = Number(document.peganotas.camponota2.value)
+    var nota3 = Number(document.peganotas.camponota3.value)
+    var nota4 = Number(document.peganotas.camponota4.value)
+    
+    
+    
+    if (recmedia == "mais") {
+        
+    } else {
+        
+    }
+}
+
